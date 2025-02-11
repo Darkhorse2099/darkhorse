682a725144b54db285f5a1fe26cb09c92f9b41de
olivesmith0209 <olivesmith0209@gmail.com>
2025-02-11 23:08:27 +0900

    init

---
+++ constants/constants.js
@@ -0,0 +1,74 @@
+export const NAME = 'Dark horse';
+
+export const DESIGNATION = `MERN Stack Developer | Open Source Contributor | Tech Blogger`;
+
+export const DETAILS = {
+  Residence: 'United States',
+  City: 'Log Angels',
+  Age: '30',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '90%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  {
+    title: 'Next Developer',
+    level: '99%',
+  },
+  {
+    title: 'Backend Developer',
+    level: '90%',
+  },
+  {
+    title: 'Unity 3D Professional Developer',
+    level: '75%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  'JavaScript',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Three.js',
+  'Next UI', 
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+  'Unity',
+  'C#',
+  'OpenAI'
+];
+
+export const CONTACTS = {
+  EMAIL: 'darkhorse950113@gmail.com',
+  //PHONE: '+923159591822',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/darkhorse950113',
+  // Alias 'https://www.linkedin.com/in/darkhorse950113/',
+  // TWITTER: 'https://twitter.com/darkhorse950113',
+  // FACEBOOK: 'https://www.facebook.com/darkhorse950113',
+};
