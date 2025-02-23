ed2a1a987deb938bc1b565e5b920bd687f57b09c
darkhorse <darkhorse950113@gmail.com>
2025-02-23 20:42:06 +0900

    remove banner

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,6 @@ import ClientReviews from "../components/HomeComponents/ClientReviews/ClientRevi
 const home = () => {
   return (
     <div className="Home-Page -z-10">
-      <Banner />
       <MyExpertise />
       <Recommendations />
       <ClientReviews />
